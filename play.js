efd6414d22197b08fadff1771f0477c38fd15466
Ben Washburn <benrobertwashburn@gmail.com>
2024-02-27 00:00:55 -0700

    worked on grid and square class

--- play.js
+++ play.js
@@ -4,31 +4,38 @@ class Grid {
         this.height = height;
         this.width = width;
         this.grid = [];
-        for (let y = 0; i < this.height; i++){
+        for (let y = 0; y < this.height; y++){
             this.grid.push([]);
-            for (let x=0; i<this.width; i++){
-                this.grid[i].push(0);
+            for (let x=0; x<this.width; x++){
+                this.grid[y].push(0);
             }
         }
     }
 
     getSquare(x, y) {
-        if((x>=0 && x<this.width) && (y>=0 && y<this.height)){
+        if(this.checkSquare(x, y)){
             return this.grid[y][x];
         } else {
-            return undefined
+            return false
         }
     }
 
-    setSquare(x, y, value) {
-        if((x>=0 && x<this.width) && (y>=0 && y<this.height)){
+    setSquare(x, y, value){
+        if(this.checkSquare(x,y)){
             this.grid[y][x] = value;
             return true
         } else{
-            return undefined
+            return false
         }
-
     }
+
+    checkSquare(x, y){
+        if((x>=0 && x<this.width) && (y>=0 && y<this.height)){
+            return true
+        } else{
+            return false;
+        }
+}
 }
 
 class Square {
@@ -37,39 +44,75 @@ class Square {
         this.x = x;
         this.y = y;
         this.grid = grid;
-        grid.setSquare(x, y, self);
+        grid.setSquare(x, y, this);
     }
 
-    move(direction) {
+    up_key(){
+        this.direction_control("up")
+    }
+
+    down_key(){
+        this.direction_control("down")
+    }
+
+    left_key(){
+        this.move_control(this.x-1, this.y, "left")
+    }
+
+    right_key(){
+        this.move_control(this.x+1, this.y, "right")
+    }
+
+    direction_control(direction){
         if(direction == "up"){
-            if (this.checkSquare(this.x, this.y-1, this.grid)){
-                // move up
-            }
-        }
-        else if(direction == "down"){
-            if (this.checkSquare(this.x, this.y+1, this.grid)){
-                // move down
-            }
-        }
-        else if(direction == "left"){
-            if (this.checkSquare(this.x-1, this.y, this.grid)){
-                // move left
-            }
-        }
-        else if(direction == "right"){
-            if (this.checkSquare(this.x+1, this.y, this.grid)){
-                // move right
-            }
+            this.move_control(this.x, this.y-1, direction)
+        } else if(direction == "down"){
+            console.log("move down")
+            this.move_control(this.x, this.y+1, direction)
+        } else if(direction == "left"){
+            this.move_control(this.x-1, this.y, direction)
+        } else if(direction == "right"){
+            this.move_control(this.x+1, this.y, direction)
         }
     }
 
-    checkSquare(x, y, grid) {
-        if(grid.getSquare){
+    collision(other){
+        if(other.number == this.number){
             return true;
-        } else {
+        } else{
             return false;
         }
     }
+
+    move_control(x, y, direction) {
+            if (this.grid.checkSquare(x, y)){
+                let other = this.grid.getSquare(x, y)
+                console.log(other)
+                if (other === 0){
+                    this.move(x, y, direction)
+                }
+                else if (this.collision(other)){
+                    this.combine(other)
+                }
+            }
+       
+    }
+
+
+    move(x, y, direction){
+        this.grid.setSquare(this.x, this.y, 0)
+        this.x = x
+        this.y = y
+        this.grid.setSquare(this.x, this.y, this);
+        this.direction_control(direction)
+    }
+
+    combine(other) {
+        other.number *=2;
+        this.grid.setSquare(this.x, this.y, 0);
+    }
+    
+
 
 
 }
